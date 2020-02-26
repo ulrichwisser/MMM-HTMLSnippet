49e68fee525f1bec3a689fbe1a95aaf3f0f0631b
Ulrich Wisser <ulrich.wisser@iis.se>
2020-02-26 14:45:42 +0100

    use self instead of this

--- MMM-HTMLSnippet.js
+++ MMM-HTMLSnippet.js
@@ -4,77 +4,64 @@
 * By ulrichwisser
 */
 
-const DefaultWidgetFormat = {
-    url: "",
-    position: "top_left",
-    width: "200px",
-    height: "200px",
-    backgroundColor: "#000"
-}
-
-
 Module.register("MMM-HTMLSnippet",{
-    defaults: {
-        html: "",
-    },
+	defaults: {
+		html: "",
+	},
 
-    start: function() {
-        let self = this;
+	start: function() {
+		let self = this;
 
-        // Schedule update timer.
-        this.scheduleUpdate(2000);
-    },
+		// Schedule update timer.
+		this.scheduleUpdate(2000);
+	},
 
 	scheduleUpdate: function(delay) {
-		let nextLoad = this.config.updateInterval;
+		let self = this
+		let nextLoad = self.config.updateInterval;
 		if (typeof delay !== "undefined" && delay >= 0) {
 			nextLoad = delay;
 		}
 		setTimeout(function() {
-			this.updateDom();
-			this.scheduleUpdate();
+			self.updateDom();
+			self.scheduleUpdate();
 		}, nextLoad);
 	},
 
-    notificationReceived: function(notification, payload, sender) {
-        if (notification == "DOM_OBJECTS_CREATED") {
-            this.sendSocketNotification("START")
-        }
-    },
+	getDom: function() {
+		let self = this
+		var wrapper = document.createElement("div")
+		wrapper.id = "HTMLSNIPPET"
+		wrapper.className = "htmlsnippet module"
+		wrapper.style.width = self.config.width
+		wrapper.style.height = self.config.height
+		wrapper.style.border = "none"
+		wrapper.style.display = "block"
+		wrapper.style.overflow = "hidden"
+		wrapper.style.backgroundColor = self.config.backgroundColor
+		wrapper.scrolling = "no"
+		wrapper.innerHTML = self.config.html
 
-    getDom: function() {
-        var wrapper = document.createElement("div")
-        wrapper.id = "HTMLSNIPPET"
-        wrapper.className = "htmlsnippet module"
-        wrapper.style.width = this.config.width
-        wrapper.style.height = this.config.height
-        wrapper.style.border = "none"
-        wrapper.style.display = "block"
-        wrapper.style.overflow = "hidden"
-        wrapper.style.backgroundColor = this.config.backgroundColor
-        wrapper.scrolling = "no"
-        wrapper.innerHTML = this.config.html
+		return wrapper
+	},
 
-        return wrapper
-    },
+	suspend: function() {
+		var doms = document.getElementsByClassName("htmlsnippet")
+		if (doms.length > 0) {
+			for (let dom of doms) {
+				dom.style.display = "none"
+			}
+		}
+	},
 
-    suspend: function() {
-        var doms = document.getElementsByClassName("htmlsnippet")
-        if (doms.length > 0) {
-            for (let dom of doms) {
-                dom.style.display = "none"
-            }
-        }
-    },
-
-    resume: function() {
-        var doms = document.getElementsByClassName("htmlsnippet")
-        if (doms.length > 0) {
-            for (let dom of doms) {
-                dom.style.display = "block"
-            }
-        }
-    },
+	resume: function() {
+		var doms = document.getElementsByClassName("htmlsnippet")
+		if (doms.length > 0) {
+			for (let dom of doms) {
+				dom.style.display = "block"
+			}
+		}
+	},
 
 }
 )
