862d96f48363bc9cc3f04c2daea69d20aa8cdddf
Ulrich Wisser <ulrich@wisser.se>
2024-03-25 15:29:19 +0100

    - Add support for several iframes in one position - fixed update on first run error

--- MMM-HTMLSnippet.js
+++ MMM-HTMLSnippet.js
@@ -1,74 +1,94 @@
 /* Magic Mirror
-* Module: MMM-HTMLSnippet
-*
-* By ulrichwisser
-*/
+ * Module: MMM-HTMLSnippet
+ *
+ * By ulrichwisser
+ */
 
-Module.register("MMM-HTMLSnippet",{
-	defaults: {
-		html: "",
-	},
+Module.register("MMM-HTMLSnippet", {
 
-	start: function() {
-		let self = this;
+    start: function() {
+        // send config to node helper
+        this.sendSocketNotification("INIT", this.config)
+    },
 
-		// send config to node helper
-		this.sendSocketNotification("INIT", this.config)
+    scheduleUpdate: function(delay) {
+        let nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+            nextLoad = delay;
+        }
+        if (nextLoad == 0) {
+            return;
+        }
+        setTimeout(function() {
+            this.updateDom();
+            this.scheduleUpdate();
+        }, nextLoad);
+    },
 
-		// Schedule update timer.
-		this.scheduleUpdate(2000);
-	},
+    getDom: function() {
+        var wrapper = document.createElement("div");
+        wrapper.setAttribute("timestamp", new Date().getTime()); // make element unique so mm doesn't ignore our update
 
-	scheduleUpdate: function(delay) {
-		let self = this
-		let nextLoad = self.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		if (nextLoad == 0) {
-			return;
-		}
-		setTimeout(function() {
-			self.updateDom();
-			self.scheduleUpdate();
-		}, nextLoad);
-	},
+        if (!this.loaded) {
+            wrapper.innerHTML = "Loading connections ...";
+            wrapper.className = "dimmed light small";
+            return wrapper;
+        }
 
-	getDom: function() {
-		let self = this
-		var wrapper = document.createElement("iframe")
-		wrapper.id = "HTMLSNIPPET-" + self.config.ident 
-		wrapper.className = "htmlsnippet module"
-		wrapper.style.width = self.config.width
-		wrapper.style.height = self.config.height
-		wrapper.style.border = "none"
-		wrapper.style.display = "block"
-		wrapper.style.overflow = "hidden"
-		wrapper.style.backgroundColor = self.config.backgroundColor
-		wrapper.style.color = self.config.color
-		wrapper.scrolling = "no"
-		wrapper.src = '/HTMLSNIPPET-' + self.config.ident
+        // add iframes
+        for (var i = 0; i < this.config.frames.length; i++) {
 
-		return wrapper
-	},
+            // initialize iframe
+            var iframe = document.createElement("iframe")
+            iframe.id = "HTMLSNIPPET-" + this.config.ident + "-" + i;
+            iframe.className = "htmlsnippet module";
+            iframe.style.width = this.config.width;
+            iframe.style.height = this.config.height;
+            iframe.style.border = "none";
+            iframe.style.overflow = "hidden";
+            iframe.style.backgroundColor = this.config.backgroundColor;
+            iframe.style.color = this.config.color;
+            iframe.scrolling = "no";
 
-	suspend: function() {
-		var doms = document.getElementsByClassName("htmlsnippet")
-		if (doms.length > 0) {
-			for (let dom of doms) {
-				dom.style.display = "none"
-			}
-		}
-	},
+            // depending on info given we need to set different src
+            if (this.config.frames[i].src !== undefined) {
+                iframe.src = this.config.frames[i].src;
+            } else if (this.config.frames[i].html !== undefined) {
+                iframe.src = '/' + iframe.id;
+            } else {
+                console.log("Error: frame " + i + " has neither src nor html.");
+                wrapper.innerHTML = "<h1>Frame " + i + " has neither src nor html.</h1>";
+                return wrapper;
+            }
+            wrapper.appendChild(iframe);
+        }
+        // done
+        return wrapper;
+    },
 
-	resume: function() {
-		var doms = document.getElementsByClassName("htmlsnippet")
-		if (doms.length > 0) {
-			for (let dom of doms) {
-				dom.style.display = "block"
-			}
-		}
-	},
+    suspend: function() {
+        var doms = document.getElementsByClassName("htmlsnippet")
+        if (doms.length > 0) {
+            for (let dom of doms) {
+                dom.style.display = "none"
+            }
+        }
+    },
 
-}
-)
+    resume: function() {
+        var doms = document.getElementsByClassName("htmlsnippet")
+        if (doms.length > 0) {
+            for (let dom of doms) {
+                dom.style.display = "block"
+            }
+        }
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "INIT_DONE") {
+            this.loaded = true;
+            this.updateDom(); // update page
+            this.scheduleUpdate(); // start update schedule
+        }
+    }
+})
--- node_helper.js
+++ node_helper.js
@@ -4,12 +4,22 @@ var NodeHelper = require("node_helper")
 
 module.exports = NodeHelper.create({
 
-	socketNotificationReceived: function(notification, payload) {
-		if (notification == "INIT") {
-                this.expressApp.use(bodyParser.json())
-                this.expressApp.use(bodyParser.urlencoded({extended: true}))
-                this.expressApp.get("/HTMLSNIPPET-"+payload.ident, (req, res) => { res.status(200).send(payload.html) })
+    socketNotificationReceived: function(notification, payload) {
+        if (notification == "INIT") {
+            this.expressApp.use(bodyParser.json());
+            this.expressApp.use(bodyParser.urlencoded({ extended: true }));
 
-		}
-  	},
+            // frames with html defined will retrieve data from the mothership
+            for (var i = 0; i < payload.frames.length; i++) {
+                if (payload.frames[i].html !== undefined) {
+                    var index = i // conserve value in local variable, needed for enclosure to work
+                    this.expressApp.get("/HTMLSNIPPET-" + payload.ident + "-" + index, (req, res) => {
+						res.status(200).send(payload.frames[index].html); });
+                }
+            }
+
+            // initialization done
+            this.sendSocketNotification('INIT_DONE', { });
+        }
+    },
 })
